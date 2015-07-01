c2dd99e8a73c3f6c77009c7db552df11817dc8fe
Yegor Litvyakov <egor@emply.ru>
2015-07-01 17:18:22 +0300

    Issue #15 Вставить форму регистрации
    Подключение библиотеки bem-forms
    Подключение технологии stylus

--- frontend/.bem/level.js
+++ frontend/.bem/level.js
@@ -1,30 +1,37 @@
 exports.getTechs = function () {
-	var techs = {
-		'bemjson.js': 'v2/bemjson.js',
-		'bemdecl.js': 'v2/bemdecl.js',
-		'deps.js': 'v2/deps.js',
-		'bemhtml': '../../bem-core/.bem/techs/bemhtml.js',
+    var techs = {
+        'bemjson.js': 'v2/bemjson.js',
+        'bemdecl.js': 'v2/bemdecl.js',
+        'deps.js': 'v2/deps.js',
+        'bemhtml': '../../bem-core/.bem/techs/bemhtml.js',
+        'stylus': '../../node_modules/bem/lib/techs/v2/styl.js',
 
-		'css': 'v2/css',
-		'html': '../../bem-core/.bem/techs/html.js',
-		'priv.js': '../../.bem/techs/priv.js.js',
+        'css': 'v2/css',
+        'html': '../../bem-core/.bem/techs/html.js',
+        'priv.js': '../../.bem/techs/priv.js.js',
 
-		'js': 'v2/js',
-		'vanilla.js': '../../bem-core/.bem/techs/vanilla.js',
-		'browser.js': '../../bem-core/.bem/techs/browser.js',
-		'browser.js+bemhtml': '../../bem-core/.bem/techs/browser.js+bemhtml.js'
-	};
+        'js': 'v2/js',
+        'vanilla.js': '../../bem-core/.bem/techs/vanilla.js',
+        'browser.js': '../../bem-core/.bem/techs/browser.js',
+        'browser.js+bemhtml': '../../bem-core/.bem/techs/browser.js+bemhtml.js'
+    };
 
-	return techs;
+    return techs;
 }
 
 exports.getConfig = function () {
-	return {
-		bundleBuildLevels: this.resolvePaths([
-			'../../bem-core/common.blocks',
-			'../../bem-core/desktop.blocks',
-			'../../common.blocks',
-			'../../desktop.blocks'
-		])
-	};
+    return {
+        bundleBuildLevels: this.resolvePaths([
+            '../../bem-core/common.blocks',
+            '../../bem-core/desktop.blocks',
+            '../../bem-forms/libs/bem-components/common.blocks',
+            '../../bem-forms/libs/bem-components/desktop.blocks',
+            '../../bem-forms/libs/bem-components/design/common.blocks',
+            '../../bem-forms/libs/bem-components/design/desktop.blocks',
+            '../../bem-forms/common.blocks',
+            '../../bem-forms/design/common.blocks',
+            '../../common.blocks',
+            '../../desktop.blocks'
+        ])
+    };
 }
--- frontend/.bem/make.js
+++ frontend/.bem/make.js
@@ -1,47 +1,59 @@
-process.env.YENV = 'production';
+//process.env.YENV = 'production';
 
 var PATH = require('path');
 
 MAKE.decl('Arch', {
-	blocksLevelsRegexp: /^.+?\.blocks/,
-	bundlesLevelsRegexp: /^.+?\.bundles/,
-	getLibraries: function () {
-		return {
-			'bem-core': {
-				type: 'git',
-				url: 'git@github.com:JIoJIaJIu/bem-core.git',//'git@github.com:bem/bem-core.git',
-				treeish: 'v2'
-			}
-		}
-	}
+    blocksLevelsRegexp: /^.+?\.blocks/,
+    bundlesLevelsRegexp: /^.+?\.bundles/,
+    getLibraries: function () {
+        return {
+            'bem-core': {
+                type: 'git',
+                url: 'git@github.com:JIoJIaJIu/bem-core.git',//'git@github.com:bem/bem-core.git',
+                treeish: 'v2'
+            },
+            'bem-forms': {
+                type: 'git',
+                url: 'git@github.com:bem/bem-forms.git',
+                treeish: 'master'
+            }
+        }
+    }
 });
 
 MAKE.decl('BundleNode', {
-	getTechs: function () {
-		return [
-			'bemjson.js',
-			'bemdecl.js',
-			'deps.js',
-			'bemhtml',
+    getTechs: function () {
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'bemhtml',
+            'stylus',
 
-			'css',
-			'html',
-			'priv.js',
+            'css',
+            'html',
+            'priv.js',
 
-			'vanilla.js',
-			'browser.js',
-			'browser.js+bemhtml'
-		];
-	},
+            'vanilla.js',
+            'browser.js',
+            'browser.js+bemhtml'
+        ];
+    },
 
-	getLevelsMap: function () {
-		return {
-			desktop: [
-				'bem-core/common.blocks',
-				'bem-core/desktop.blocks',
-				'common.blocks',
-				'desktop.blocks'
-			]
-		};
-	}
+    getLevelsMap: function () {
+        return {
+            desktop: [
+                'bem-core/common.blocks',
+                'bem-core/desktop.blocks',
+                'bem-forms/common.blocks',
+                'bem-forms/design/common.blocks',
+                'bem-forms/libs/bem-components/common.blocks',
+                'bem-forms/libs/bem-components/desktop.blocks',
+                'bem-forms/libs/bem-components/design/common.blocks',
+                'bem-forms/libs/bem-components/design/desktop.blocks',
+                'common.blocks',
+                'desktop.blocks'
+            ]
+        };
+    }
 });
